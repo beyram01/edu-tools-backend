9c3d5277607b43837e920a403e51fccc39863b1a
beyram01 <beyramjouini7@gmail.com>
2021-03-19 18:07:08 +0100

    first commit

--- config/database.js
+++ config/database.js
@@ -1,35 +1,17 @@
 module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "development") {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "mongoose",
-          settings: {
-            client: "mongo",
-            uri: "mongodb://localhost:27017",
-          },
-          options: {
-            useNullAsDefault: true,
-          },
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "mongoose",
+        settings: {
+          client: "mongo",
+          uri: env("DATABASE_URI"),
+        },
+        options: {
+          ssl: true,
         },
       },
-    };
-  } else {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "mongoose",
-          settings: {
-            client: "mongo",
-            uri: env("DATABASE_URI"),
-          },
-          options: {
-            ssl: true,
-          },
-        },
-      },
-    };
-  }
+    },
+  };
 };
