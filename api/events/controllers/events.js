276d3bfd14d05284c1e36b0bda68e83f2f0f2878
beyram01 <beyramjouini7@gmail.com>
2021-03-25 18:34:38 +0100

    adding events model

---
+++ api/events/controllers/events.js
@@ -0,0 +1,21 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  async find(ctx) {
+    const { username } = ctx.params;
+    let entitys;
+    if (username) {
+      entitys = await strapi.services.events.find({
+        "users_permissions_user.username": username,
+      });
+    } else {
+      entitys = await strapi.services.events.find();
+    }
+    return sanitizeEntity(entitys, { model: strapi.models.events });
+  },
+};
